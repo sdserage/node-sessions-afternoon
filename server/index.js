36b7c99ce2e4062690e87c557e895b0fc451c07c
Scott Serage <xttocsurge51@gmail.com>
2017-09-26 11:59:55 -0600

    Completed Step 2

--- server/index.js
+++ server/index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+
+const app = express(); // Creates an express app
+app.use(bodyParser.json()); // Middleware that uses the body-parser
+app.use(session({
+  secret: 'tH3 $3Kr3T 1$ Th@ t3h mEs$@Ge 1s $tIlL r3@dA8L3', // key used for incryption
+  resave: false, // 
+  saveUninitialized: false //
+}));
