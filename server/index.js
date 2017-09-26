1f3dc62600aaaefe051d0858aa6221c6058d508e
Scott Serage <xttocsurge51@gmail.com>
2017-09-26 12:08:47 -0600

    Actually completed Step 2, I had forgotten to listen on port 3000 (In my defense, the normal instructions don't tell you to do this, only the summary and the detailed instructions do).

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express(); // Creates an express app
 app.use(bodyParser.json()); // Middleware that uses the body-parser
 app.use(session({
   secret: 'tH3 $3Kr3T 1$ Th@ t3h mEs$@Ge 1s $tIlL r3@dA8L3', // key used for incryption
-  resave: false, // 
+  resave: false, //
   saveUninitialized: false //
 }));
+
+const port = 3000;
+app.listen(port, () => {console.log(`Server listening ${port.toLocaleString()} under the sea...`)}) // This should be normally called after the database is aquired. toLocaleString is for fun.
